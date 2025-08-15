45124a72689c88e27fcc74659fbfdab963433930
notweuz <weuz.main@gmail.com>
2025-08-15 23:10:50 +0400

    Change border to 1.3k

--- src/main/java/ru/ntwz/worldborder/config/BorderConfig.java
+++ src/main/java/ru/ntwz/worldborder/config/BorderConfig.java
@@ -2,5 +2,5 @@ package ru.ntwz.worldborder.config;
 
 public class BorderConfig {
     public static final int SOUND_PER_TICKS = 15;
-    public static final double BORDER_SIZE = 1500.0;
+    public static final double BORDER_SIZE = 1300.0;
 }
